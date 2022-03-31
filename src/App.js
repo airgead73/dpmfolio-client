939dad5913b5825ba3420449a3aec21fc8a351ac
Dan Moneypenny <catullus1973@gmail.com>
2022-03-31 07:03:53 -0700

    update App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>DPMFOLIO</h1>
-        <p><small><a href="/authors" target="_blank">authors</a></small></p>
+        <p><small><a href="http://localhost:6060/api/authors" target="_blank">authors</a> <a href="http://localhost:6060/api/books" target="_blank">books</a></small></p>
       </header>
     </div>
   );
