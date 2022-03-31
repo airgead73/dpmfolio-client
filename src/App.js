4a324c19f1a4137e27ceabb4fae9086df9b8390d
Dan Moneypenny <catullus1973@gmail.com>
2022-03-31 01:57:41 -0700

    update App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>DPMFOLIO</h1>
-        <p>db test</p>
+        <p>session test</p>
       </header>
     </div>
   );
