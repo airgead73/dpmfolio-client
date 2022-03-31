abab5c03a6ed776d10dcf8eb51975362f68a0ff7
Dan Moneypenny <catullus1973@gmail.com>
2022-03-31 07:15:38 -0700

    update App

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>DPMFOLIO</h1>
-        <p>local: <small><a href="http://localhost:6060/api/authors" target="_blank">authors</a> <a href="http://localhost:6060/api/books" target="_blank">books</a></small></p>
-        <p>heroku: <small><a href="/api/authors" target="_blank">authors</a> <a href="/api/books" target="_blank">books</a></small></p>
+        <p>local:</p>
+        <ul>
+          <li><a href="http://localhost:6060/api/authors" target="_blank">authors</a></li>
+          <li><a href="http://localhost:6060/api/authors/123" target="_blank">author</a></li>
+          <li><a href="http://localhost:6060/api/books" target="_blank">books</a></li>
+          <li><a href="http://localhost:6060/api/books/123" target="_blank">book</a></li>
+        </ul>
+        <p>heroku:</p>
+        <ul>
+          <li><a href="/api/authors" target="_blank">authors</a></li>
+          <li><a href="/api/authors/123" target="_blank">authors</a></li>
+          <li><a href="/api/books" target="_blank">books</a></li>
+          <li><a href="/api/books/123" target="_blank">book</a></li>
+        </ul>
       </header>
     </div>
   );
