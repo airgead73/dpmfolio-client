9fba5ce44dae9eafd24c85e69ec144c2729f5d39
Dan Moneypenny <catullus1973@gmail.com>
2022-03-31 00:59:13 -0700

    update App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>DPMFOLIO</h1>
-        <p>heroku test</p>
+        <p>db test</p>
       </header>
     </div>
   );
