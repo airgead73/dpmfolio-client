f8c4e9536c490a0a9764102e9ddfed1aa66db7bc
Dan Moneypenny <catullus1973@gmail.com>
2022-03-31 07:07:53 -0700

    update App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>DPMFOLIO</h1>
-        <p><small><a href="http://localhost:6060/api/authors" target="_blank">authors</a> <a href="http://localhost:6060/api/books" target="_blank">books</a></small></p>
+        <p>local: <small><a href="http://localhost:6060/api/authors" target="_blank">authors</a> <a href="http://localhost:6060/api/books" target="_blank">books</a></small></p>
+        <p>heroku: <small><a href="/api/authors" target="_blank">authors</a> <a href="/api/books" target="_blank">books</a></small></p>
       </header>
     </div>
   );
