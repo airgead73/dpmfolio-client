b27da2dda4c4a81213e1997573cdf4b5cf5bbe14
Dan Moneypenny <catullus1973@gmail.com>
2022-04-01 01:16:40 -0700

    update profile component

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Profile = () => {
+
+  const { user } = useAuth0();
+  const { name, picture, email } = user;
+
   return (
     <div>
       <h2>profile</h2>
+      <div>
+        <img src={picture} alt="Profile" title="Profile" />
+      </div>
+      <div>
+        <h3>{name}</h3>
+        <p>{email}</p>
+      </div>
+      <div>
+        <pre>
+          {JSON>stringify(user, null, 2)}
+        </pre>
+      </div>
     </div>
   );
 }
